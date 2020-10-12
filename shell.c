c8eca757ae9552b6881c931a6b56a9457cbceb47
Ikechukwu <ikechukwuanude@gmail.com>
2020-10-12 20:44:31 +0000

    editing the shell file to rint the current working directory

--- shell.c
+++ shell.c
@@ -2,85 +2,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
 
 #define MAX_COMMAND_LINE_LEN 1024
 #define MAX_COMMAND_LINE_ARGS 128
 
-char prompt[] = "shell > ";
+char prompt[] = "> ";
 char delimiters[] = " \t\r\n";
-char **environ;
+extern char **environ;
 
-int main() 
-{
+
+
+int main() {
     // Stores the string typed into the command line.
     char command_line[MAX_COMMAND_LINE_LEN];
-
+    char cmd_bak[MAX_COMMAND_LINE_LEN];
+  
     // Stores the tokenized command line input.
     char *arguments[MAX_COMMAND_LINE_ARGS];
-    
-    while (true) 
-    {
-        // Print the shell prompt.
-        printf("%s", prompt);
-        fflush(stdout);
+    	
+    while (true) {
+      
+        do{ 
+            // Print the shell prompt.
+            printf("%s", prompt);
+            fflush(stdout);
+
+            // Read input from stdin and store it in command_line. If there's an
+            // error, exit immediately. (If you want to learn more about this line,
+            // you can Google "man fgets")
         
-        // Read input from stdin and store it in command_line. If there's an
-        // error, exit immediately. (If you want to learn more about this line,
-        // you can Google "man fgets")
-        if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) 
-        {
-            fprintf(stderr, "fgets error");
-            exit(0);
-        }
+            if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
+                fprintf(stderr, "fgets error");
+                exit(0);
+            }
+ 
+        }while(command_line[0] == 0x0A);  // while just ENTER pressed
+
+      
         // If the user input was EOF (ctrl+d), exit the shell.
-        if (feof(stdin)) 
-        {
+        if (feof(stdin)) {
             printf("\n");
             fflush(stdout);
             fflush(stderr);
             return 0;
-        }      
+        }
+
         // TODO:
-        // 1. Tokenize the command line input (split it on whitespace)
-        int index = 0;
-        arguments[index] = strtok(command_line, delimiters);
-        while (arguments[index] != NULL)
-        {
-          arguments[index] = strtok(command_line, delimiters);
-          index++;
-        }
-      
-        // 2. Create a child process which will execute the command line input
-        int chld = fork();
-        if(chld == 0) 
-        {
-          if(execve(arguments[0], arguments, environ) == -1)//execute command with parametr
-          {
-            printf("%s: Command not found.\n", arguments[0]);//returns error if command not found
-            return -1;
-           }
-          return 0;
-        } 
-           // 3. The parent process should wait for the child to complete
-        else if(chld > 0)
-        {
-          wait(NULL);
-        } else 
-        {
-          return -1;
-        }
-      
-   
         // 
+        
+			  // 0. Modify the prompt to print the current working directory
+        
+			  
+			
+        // 1. Tokenize the command line input (split it on whitespace)
+
+      
+        // 2. Implement Built-In Commands
+      
+    
+        // 3. Create a child process which will execute the command line input
+
+  
+        // 4. The parent process should wait for the child to complete unless its a background process
+      
+      
         // Hints (put these into Google):
         // man fork
-        // man execve
+        // man execvp
         // man wait
         // man strtok
+        // man environ
+        // man signals
+        
+        // Extra Credit
+        // man dup2
+        // man open
+        // man pipes
     }
-    
     // This should never be reached.
     return -1;
 }
-    
-   
